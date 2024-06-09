9cc2cda4e88151a09c56e7aaa75d628048d33009
ykwizera <144004543+ykwizera@users.noreply.github.com>
2024-06-09 19:18:24 +0200

    new

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Fabric script that distributes an archive to your web servers"""
+from fabric.api import env, put, run
+from os.path import exists
+
+env.hosts = ['34.204.36.196', '34.204.36.196']
+env.user = "ubuntu"
+env.key = "~/.ssh/id_rsa"
+
+
+def do_deploy(archive_path):
+    """Function to distribute an archive to your web servers"""
+    if not exists(archive_path):
+        return False
+    try:
+        file_name = archive_path.split("/")[-1]
+        name = file_name.split(".")[0]
+        path_name = "/data/web_static/releases/" + name
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}/".format(path_name))
+        run('tar -xzf /tmp/{} -C {}/'.format(file_name, path_name))
+        run("rm /tmp/{}".format(file_name))
+        run("mv {}/web_static/* {}".format(path_name, path_name))
+        run("rm -rf {}/web_static".format(path_name))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}/ /data/web_static/current'.format(path_name))
+        return True
+    except Exception:
+        return False
